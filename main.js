6e09c625c10e25848d9a426c980fe637d7dc1a4d
Hitesh Mandav <mandavhitesh@gmail.com>
2021-09-30 01:05:41 +0530

    Added start stop and next level funtionality

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+
+
 var score = 0;
 
 var lives = 3;
 
-var x = canvas.width / 2;
+var gameStatus = 'none';
+var level = 1;
+
+var x = (canvas.width) * Math.random();
 var y = canvas.height - 30 ;
 var dx = 2;
 var dy = -2;
@@ -26,6 +31,7 @@ var brickHeight = 20;
 var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
+var ballIntervel;
 
 var bricks = [];
 for(var c=0; c<brickColumnCount; c++) {
@@ -35,18 +41,66 @@ for(var c=0; c<brickColumnCount; c++) {
     }
 }
 
+function setbrickStatus(){
+    for(var c=0; c<brickColumnCount; c++) {
+        bricks[c] = [];
+        for(var r=0; r<brickRowCount; r++) {
+            bricks[c][r] = { x: 0, y: 0 , status: 1};
+        }
+    }
+}
+
+// 
+
 var intervel = setInterval(draw,10);
 
+// document.getElementById("successStatus").innerHTML = "Press start Game button at the top";
+// LevelChanged
+function start() {
+    level=1;
+    setbrickStatus();
+    intervel = setInterval(draw,10);
+    document.getElementById("successStatus").innerHTML = "Game in Progress level "+level;
+    gameStatus = 'started';
+   if(!ballIntervel) {
+    ballIntervel = setInterval(()=>{
+        x += dx;
+        y += dy;
+        // console.log(`fjflks:: ${x} , ${y}`)
+    },10);
+   }
+        
+}
+
+function changeLevel() {
+    setbrickStatus();
+    intervel = setInterval(draw,10);
+    document.getElementById("successStatus").innerHTML = "Game in Progress level "+level;
+    document.getElementById("cLButton").disabled = true;
+     ballIntervel = setInterval(()=>{
+         x += dx;
+         y += dy;
+         // console.log(`fjflks:: ${x} , ${y}`)
+     },10);
+         
+}
+
+function stop() {
+    console.log(`stop}`)
+    clearInterval(ballIntervel);
+    clearInterval(intervel);
+ 
+}
 
 function drawScore() {
     ctx.font = "16px Arial";
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "#FFFFFF";
     ctx.fillText("Score: "+score, 8, 20);
 }
 
 function drawLives() {
     ctx.font = "16px Arial";
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "#FFFFFF";
     ctx.fillText("Lives: "+lives, canvas.width-65, 20);
 }
 
@@ -86,10 +140,14 @@ function collisionDetection() {
                     dy = -dy;
                     b.status = 0;
                     score++;
-                    if(score == brickRowCount*brickColumnCount) {
-                        alert("YOU WIN, CONGRATULATIONS!");
-                        document.location.reload();
-                        clearInterval(intervel); // Needed for Chrome to end game
+                    if(score/level == (brickRowCount*brickColumnCount)) {
+                        // alert("YOU WIN, CONGRATULATIONS!");
+                        document.getElementById("successStatus").innerHTML = `YOU WON Level : ${level}. CONGRATULATIONS!! Press button to change level.`;
+                        gameStatus="EndGame_win"
+                        // document.location.reload();
+                        clearInterval(intervel);
+                        clearInterval(ballIntervel); // Needed for Chrome to end game
+                        
                     }
                 }
             }
@@ -100,7 +158,7 @@ function collisionDetection() {
 function drawBall(){
     ctx.beginPath();
     ctx.arc(x,y, ballRadius, 0 , Math.PI*2, true)
-    ctx.fillStyle = "rgba(91, 167, 222, 1)";
+    ctx.fillStyle = "rgba(255,255,255, 1)";
     ctx.fill();
     ctx.closePath();
 }
@@ -108,7 +166,7 @@ function drawBall(){
 function drawPaddle(){
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "rgba(91, 167, 222, 1)";
+    ctx.fillStyle = "rgba(255,255,255, 1)";
     ctx.fill();
     ctx.closePath();
    
@@ -131,16 +189,19 @@ function detectWall(){
         else{
             lives--;
             if(!lives) {
-                alert("GAME OVER");
-                document.location.reload();
-                clearInterval(intervel); // Needed for Chrome to end game
+                // alert("GAME OVER");
+                document.getElementById("successStatus").innerHTML = `GAME OVER with Score : ${score} at level ${level}`;
+                gameStatus="EndGame_lost"
+                // document.location.reload();
+                clearInterval(intervel);
+                clearInterval(ballIntervel);  // Needed for Chrome to end game
             }
             else {
-                x = canvas.width/2;
+                x = (canvas.width) * Math.random();
                 y = canvas.height-30;
                 dx = 2;
                 dy = -2;
-                paddleX = (canvas.width-paddleWidth)/2;
+                // paddleX = (canvas.width-paddleWidth)/2;
             }
          }
     }
@@ -165,28 +226,29 @@ function movePaddle(){
 function drawBricks() {
     for(var c=0; c<brickColumnCount; c++) {
         for(var r=0; r<brickRowCount; r++) {
-            if(bricks[c][r].status ==1 ){
+            if(bricks[c][r].status == 1 ){
                 var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
                 var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
                 bricks[c][r].x = brickX;
                 bricks[c][r].y = brickY;
                 ctx.beginPath();
                 ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                ctx.fillStyle = "#0095DD";
+                ctx.fillStyle = "#FFFFFF";
                 ctx.fill();
                 ctx.closePath();
             }
         }
     }
 }
+
 function draw(){
-   
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBricks();
     drawBall();
     detectWall();
-    x += dx;
-    y += dy;
+    // x += dx;
+    // y += dy;
     collisionDetection()
 
     movePaddle();
@@ -194,10 +256,19 @@ function draw(){
     drawPaddle();
     drawScore();
     drawLives();
-
+    if(gameStatus == 'none') {
+        document.getElementById("successStatus").innerHTML = "Press start Game button at the top";
+        document.getElementById("cLButton").disabled = true;
+    }
+    if(gameStatus == 'EndGame_win') {
+        level +=1;
+        gameStatus = 'LevelChanged'
+        // document.getElementById("successStatus").innerHTML = "Press start Game button at the top";
+        document.getElementById("cLButton").disabled = false;
+    }
+    // document.getElementById("successStatus").innerHTML = "Press start Game button at the top";
     //requestAnimationFrame(draw);
 }
-
 document.addEventListener("keydown", keyPressed, false);
 document.addEventListener("keyup", keyReleased, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
